0bf05948f248deb0b783b95ac3632c7de77b5571
HuaZhouyang <2522424515@qq.com>
2021-10-08 13:19:17 +0800

    Lesson01_GitTest: master third commit

--- Lesson01_GitTest/src/Main.java
+++ Lesson01_GitTest/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!_2");
+        System.out.println("Hello Git!_master_3");
     }
 }
